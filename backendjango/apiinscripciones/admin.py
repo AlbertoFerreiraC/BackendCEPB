453f5f8096415ded0a07c361ef787b81ad9a99f4
aferreira <aferreira@backend.myguest.virtualbox.org>
2024-06-12 20:59:39 -0400

    generacion de pagare en pdf

--- backendjango/apiinscripciones/admin.py
+++ backendjango/apiinscripciones/admin.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.utils.html import format_html  # Asegúrate de importar format_html
 from .models import Inscripcion, Arancel
 
 class InscripcionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'tutor_alumno', 'ins_fecha', 'ins_descuento', 'ins_contrato_fecha')
+    list_display = ('id', 'tutor_alumno', 'ins_fecha', 'ins_descuento', 'ins_contrato_fecha', 'generar_pagare_pdf_link')
     search_fields = ('tutor_alumno__tutor__tut_nom', 'tutor_alumno__alumno__alum_nom')
     list_filter = ('ins_fecha', 'ins_descuento')
+    actions = ['generar_pdf_action', 'generar_pagare_pdf_action']
 
     def generar_pdf_action(self, request, queryset):
         if queryset.count() != 1:
@@ -14,13 +16,22 @@ class InscripcionAdmin(admin.ModelAdmin):
             return
 
         inscripcion_id = queryset.first().id
-
-        # Redirigir a la vista para generar el PDF con el ID de la inscripción
         return HttpResponseRedirect(reverse('generar_pdf', kwargs={'id': inscripcion_id}))
-
-
     generar_pdf_action.short_description = "Generar PDF de contrato"
-    actions = ['generar_pdf_action']
+
+    def generar_pagare_pdf_action(self, request, queryset):
+        if queryset.count() != 1:
+            self.message_user(request, "Por favor seleccione exactamente una inscripción.")
+            return
+
+        inscripcion_id = queryset.first().id
+        return HttpResponseRedirect(reverse('generar_pagare_pdf', kwargs={'id': inscripcion_id}))
+    generar_pagare_pdf_action.short_description = "Generar PDF del pagaré"
+
+    def generar_pagare_pdf_link(self, obj):
+        url = reverse('generar_pagare_pdf', args=[obj.id])
+        return format_html('<a href="{}" target="_blank">Generar pagaré PDF</a>', url)
+    generar_pagare_pdf_link.short_description = 'Generar pagaré PDF'
 
 admin.site.register(Inscripcion, InscripcionAdmin)
 
--- backendjango/apiinscripciones/views.py
+++ backendjango/apiinscripciones/views.py
@@ -128,3 +128,61 @@ def generar_pdf(request, id):
     doc.build(elements)
 
     return response
+
+def generar_pagare_pdf(request, id):
+    # Obtener la inscripción correspondiente
+    inscripcion = get_object_or_404(Inscripcion, id=id)
+
+    # Obtener datos del tutor y del alumno
+    tutor = inscripcion.tutor_alumno.tutor
+    alumno = inscripcion.tutor_alumno.alumno
+    arancel = inscripcion.aranceles.first()  # Suponiendo que sólo hay un arancel asociado a la inscripción
+
+    # Asegurarse de usar los atributos correctos
+    tutor_nombre = tutor.tut_nom
+    tutor_apellido = tutor.tut_ape
+    tutor_direccion = tutor.tut_direc
+    tutor_ci = tutor.tut_ci
+    alumno_nombre = alumno.alum_nom
+    alumno_apellido = alumno.alum_ape
+    arancel_matricula = arancel.arancel_matricula
+    arancel_cuota = arancel.arancel_cuota
+    arancel_nivel = arancel.arancel_nivel
+    arancel_grado = arancel.arancel_grado
+
+    # Definir el contenido del pagaré
+    pagare_content = [
+        ("PAGARÉ", "CENTER", True),
+        (f"Yo, {tutor_nombre} {tutor_apellido}, con C.I. {tutor_ci} y domiciliado en {tutor_direccion}, me obligo a pagar a la orden del CENTRO EDUCATIVO PARAGUAY - BRASIL (CEPB) la suma de {arancel_matricula + arancel_cuota} guaraníes.", "JUSTIFY", False),
+        (f"Este pagaré corresponde al pago de la matrícula y la cuota del estudiante {alumno_nombre} {alumno_apellido}.", "JUSTIFY", False),
+        (f"La suma mencionada será pagada en cuotas mensuales de {arancel_cuota} guaraníes cada una, con vencimiento el primer día de cada mes.", "JUSTIFY", False),
+        ("En caso de mora en el pago de cualquier cuota, se aplicará un recargo del 15% mensual sobre el monto adeudado.", "JUSTIFY", False),
+        ("Lugar y Fecha", "LEFT", True),
+        ("____________________________", "LEFT", True),
+        ("Firma del Deudor", "LEFT", True),
+    ]
+
+    # Crear un objeto PDF
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename="pagare_{inscripcion.id}.pdf"'
+
+    # Crear documento PDF usando ReportLab
+    doc = SimpleDocTemplate(response, pagesize=letter)
+    elements = []
+
+    # Definir estilos
+    styles = getSampleStyleSheet()
+    style_center = ParagraphStyle(name="Center", alignment=TA_CENTER)
+    style_left = ParagraphStyle(name="Left", alignment=TA_LEFT)
+    style_justify = ParagraphStyle(name="Justify", alignment=TA_JUSTIFY)
+
+    # Agregar contenido al PDF
+    for text, alignment, is_bold in pagare_content:
+        style = style_center if alignment == "CENTER" else (style_left if alignment == "LEFT" else style_justify)
+        if is_bold:
+            style.fontName = "Helvetica-Bold"
+        elements.append(Paragraph(text, style))
+
+    doc.build(elements)
+
+    return response
