1bf41831152c95c628691c6067ab2620cce713fb
Alberto <aferreira6064@gmail.com>
2024-06-15 20:26:09 -0400

    correccion de metodo post del tutor

--- backendjango/apitutores/urls.py
+++ backendjango/apitutores/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import tutor_list, tutor_detail, tutor_alumno_list, tutor_alumno_detail
+from .views import TutorListCreate, TutorDetail, TutorAlumnoListCreate, TutorAlumnoDetail
 
 urlpatterns = [
-    path('', tutor_list, name='tutor_list'),
-    path('<int:id>/', tutor_detail, name='tutor_detail'),
-    path('alumnos/', tutor_alumno_list, name='tutor_alumno_list'),
-    path('alumnos/<int:id>/', tutor_alumno_detail, name='tutor_alumno_detail'),
+    path('', TutorListCreate.as_view(), name='tutor_list'),
+    path('<int:pk>/', TutorDetail.as_view(), name='tutor_detail'),
+    path('alumnos/', TutorAlumnoListCreate.as_view(), name='tutor_alumno_list'),
+    path('alumnos/<int:pk>/', TutorAlumnoDetail.as_view(), name='tutor_alumno_detail'),
 ]
--- backendjango/apitutores/views.py
+++ backendjango/apitutores/views.py
@@ -1,29 +1,19 @@
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
+from rest_framework import generics
 from .models import Tutor, TutorAlumno
 from .serializers import TutorSerializer, TutorAlumnoSerializer
-from rest_framework.decorators import api_view
 
-@api_view(['GET'])
-def tutor_list(request):
-    tutores = Tutor.objects.all()
-    serializer = TutorSerializer(tutores, many=True)
-    return JsonResponse(serializer.data, safe=False)
+class TutorListCreate(generics.ListCreateAPIView):
+    queryset = Tutor.objects.all()
+    serializer_class = TutorSerializer
 
-@api_view(['GET'])
-def tutor_detail(request, id):
-    tutor = get_object_or_404(Tutor, id=id)
-    serializer = TutorSerializer(tutor)
-    return JsonResponse(serializer.data, safe=False)
+class TutorDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tutor.objects.all()
+    serializer_class = TutorSerializer
 
-@api_view(['GET'])
-def tutor_alumno_list(request):
-    tutor_alumnos = TutorAlumno.objects.all()
-    serializer = TutorAlumnoSerializer(tutor_alumnos, many=True)
-    return JsonResponse(serializer.data, safe=False)
+class TutorAlumnoListCreate(generics.ListCreateAPIView):
+    queryset = TutorAlumno.objects.all()
+    serializer_class = TutorAlumnoSerializer
 
-@api_view(['GET'])
-def tutor_alumno_detail(request, id):
-    tutor_alumno = get_object_or_404(TutorAlumno, id=id)
-    serializer = TutorAlumnoSerializer(tutor_alumno)
-    return JsonResponse(serializer.data, safe=False)
+class TutorAlumnoDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TutorAlumno.objects.all()
+    serializer_class = TutorAlumnoSerializer
